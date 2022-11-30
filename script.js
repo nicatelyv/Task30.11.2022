40f3e42099ee139411a01774727f5a621d5b783c
Nicat <tu72k7cwp@code.edu.az>
2022-11-30 23:31:15 +0400

    Added

---
+++ script.js
@@ -0,0 +1,28 @@
+// TASK 1
+
+// let arr = [5, 10, 2, 7, 9, 3, 7, 6, 4, 2];
+// let reducer = (a, b) => a + b;
+// let cem = arr.reduce(reducer)
+// let say = arr.length
+// console.log(cem / say);
+
+
+
+// TASK2
+
+// let arr = [1, 5, 2, 7]
+// arr.push(11)
+// console.log(arr);
+
+
+// TASK3
+
+// let str = "Salam Necesen Neynirsen";
+// let count = 0;
+// for (let i = 0; i < str.length; i++) {
+//    if (str[i] == "n" || str[i] == "N") {
+//       count++
+//    }
+// }
+// console.log(count);
+
